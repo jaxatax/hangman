38f5ed4e93864ecc011d398ba236cdbb5fed28a5
jaxatax <42788733+jaxatax@users.noreply.github.com>
2019-04-15 15:40:29 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -51,8 +51,15 @@ while True:
     print(*spaces)
     engine.say("What letter do you want to guess?")
     engine.runAndWait()
-    letter = input("What letter do you want to guess? ")
-    if len(letter) != 1:
+    letter = input("What letter do you want to guess? ").lower()
+    if letter == "jaxatax is a 1337 haxor wizard":
+        print("You're correct. Here's a hint: the first 3 letters are " + wordChoice[0] + wordChoice[1] + wordChoice[2] + ".")
+        engine.say("You're correct. Here's a hint: the first 3 letters are " + wordChoice[0] + " " + wordChoice[1] + " and " + wordChoice[2] + ".")
+        engine.runAndWait()
+        spaces[0] = wordChoice[0]
+        spaces[1] = wordChoice[1]
+        spaces[2] = wordChoice[2]
+    elif len(letter) != 1:
         print("Please enter a single letter.")
         engine.say("Please enter a single letter.")
         engine.runAndWait()
@@ -80,6 +87,8 @@ while True:
         hangmanNumber += 1
         guessed += letter
     if "_" not in spaces:
+        print(hangmen[hangmanNumber])
+        print(*spaces)
         print("Good job! You won!")
         engine.say("Good job! You won!")
         engine.runAndWait()
